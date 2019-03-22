3db54f754fe60a5bd446b0afd914184be0d10ea1
Bia Alegre <biaalegre@bia-alegre.local>
2019-03-21 22:30:34 -0300

    refatoracao

--- PlaygroundBook/Sources/C1P2.swift
+++ PlaygroundBook/Sources/C1P2.swift
@@ -2,16 +2,16 @@ import UIKit
 import SpriteKit
 import PlaygroundSupport
 
-struct Sprite {
-    let node : SKSpriteNode
-    let animations : [SKTexture]
-}
-
-struct PhysicsCategories{
-    static let newton : UInt32 = 0x1 << 0
-    static let apple : UInt32 = 0x1 << 1
-    static let ground : UInt32 = 0x1 << 2
-}
+//struct Sprite {
+//    let node : SKSpriteNode
+//    let animations : [SKTexture]
+//}
+//
+//struct PhysicsCategories{
+//    static let newton : UInt32 = 0x1 << 0
+//    static let apple : UInt32 = 0x1 << 1
+//    static let ground : UInt32 = 0x1 << 2
+//}
 
 @objc(C1P2)
 public class C1P2: UIViewController,PlaygroundLiveViewMessageHandler, PlaygroundLiveViewSafeAreaContainer {
@@ -22,6 +22,7 @@ public class C1P2: UIViewController,PlaygroundLiveViewMessageHandler, Playground
     //tamanho do device
     var windowSize : CGSize!
     
+    var newtonSprite : Sprite!
     public override func viewDidLoad() {
         
         self.windowSize = UIScreen.main.bounds.size
@@ -34,114 +35,12 @@ public class C1P2: UIViewController,PlaygroundLiveViewMessageHandler, Playground
         
         
         //cria a cena
-        self.scene = createScene()
-        self.scene.physicsWorld.contactDelegate = self
-        //adiciona a cena na sceneview
+        //self.scene = createScene()
+        self.scene = GameScene(size: CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
         self.sceneView.presentScene(self.scene)
-        self.view.addSubview(sceneView)
-        
-
+        //self.scene.physicsWorld.contactDelegate = self
+        //adiciona a cena na sceneview
+       self.view.addSubview(sceneView)
     }
     
-    func createScene() -> SKScene {
-        
-        let scene = SKScene(size: CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
-        
-        scene.backgroundColor = #colorLiteral(red: 0.368627451, green: 0.7333333333, blue: 0.7921568627, alpha: 1)
-        print("gravidade: \(scene.physicsWorld.gravity)")
-        //scene.physicsWorld.gravity = CGVector(dx: 0, dy: -9.8)
-        
-        //carregando a arvore
-        let treeSprite = loadSprite(imageName: "tree", sizePercentage: 0.35)
-        let treeNode = treeSprite.node
-        treeNode.position = CGPoint(x: treeNode.size.width/2, y: treeNode.size.height/2)
-        //adicionando a maca na cena
-        scene.addChild(treeNode)
-        
-        //carregando sprite do newton
-        let newtonSprite = loadSprite(imageName: "newton", sizePercentage: 0.3, animated: true, frames: 1)
-        
-        let newtonNode = newtonSprite.node
-        newtonNode.position = CGPoint(x: windowSize.width * 0.6, y:  newtonNode.size.height/2)
-         newtonNode.physicsBody = SKPhysicsBody(texture: newtonSprite.animations[0], size:  newtonNode.size)
-        newtonNode.physicsBody?.affectedByGravity = false
-        //newtonNode.physicsBody?.pinned = true
-        newtonNode.physicsBody?.isDynamic = false
-        newtonNode.physicsBody?.categoryBitMask = PhysicsCategories.newton
-        //adicinando o newton na cena
-        scene.addChild(newtonNode)
-        
-        //carregando o sprite da maca
-        let appleSprite = loadSprite(imageName: "apple", sizePercentage: 0.35)
-        let appleNode = appleSprite.node
-        appleNode.position = CGPoint(x: windowSize.width * 0.6, y: treeNode.position.y * 1.2)
-        //adicionando a maca na cena
-        scene.addChild(appleNode)
-        //adicionando physics body a maca
-        appleNode.physicsBody = SKPhysicsBody(circleOfRadius: appleNode.size.width/2)
-        appleNode.physicsBody?.affectedByGravity = true
-        appleNode.physicsBody?.categoryBitMask = PhysicsCategories.apple
-        appleNode.physicsBody?.collisionBitMask = PhysicsCategories.newton | PhysicsCategories.ground
-
-        //adicionando o chao
-        let floor = SKShapeNode(rect: CGRect(x: 0, y: 0, width: windowSize.width, height: 1))
-        floor.fillColor = #colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1)
-        floor.position = CGPoint(x: 0, y: 0)
-        print("tamanho do chao: \(floor.frame.size)")
-        print("posicao do chao: \(floor.position)")
-        floor.physicsBody = SKPhysicsBody(rectangleOf: floor.frame.size, center: CGPoint(x: floor.frame.width/2, y: floor.frame.height/2))
-        floor.physicsBody?.affectedByGravity = false
-        floor.physicsBody?.isDynamic = false
-        floor.physicsBody?.categoryBitMask = PhysicsCategories.ground
-        floor.physicsBody?.collisionBitMask = PhysicsCategories.apple
-    
-
-        scene.addChild(floor)
-        
-        return scene
-        
-    }
-    
-    func loadSprite(imageName : String, sizePercentage : CGFloat, animated: Bool = false, frames: Int = 1) -> Sprite{
-        
-        var textures = [SKTexture]()
-        for i in 0..<frames{
-            let textureName = imageName + String(i) + ".png"
-            print(textureName)
-            let texture = SKTexture(imageNamed: textureName)
-            print(texture)
-            textures.append(texture)
-        }
-        
-        let spriteNode = SKSpriteNode(texture: textures[0])
-        spriteNode.scale(to: CGSize(width: spriteNode.size.width * sizePercentage, height: spriteNode.size.height * sizePercentage))
-        let sprite = Sprite(node: spriteNode, animations: textures)
-        
-        return sprite
-    }
-    
-    
-}
-
-extension C1P2 : SKPhysicsContactDelegate{
-    public func didBegin(_ contact: SKPhysicsContact) {
-        var firstBody: SKPhysicsBody
-        var secondBody: SKPhysicsBody
-        
-        if(contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask){
-            firstBody = contact.bodyA
-            secondBody = contact.bodyB
-        }else{
-            firstBody = contact.bodyB
-            secondBody = contact.bodyA
-        }
-        
-        
-        if((firstBody.categoryBitMask & PhysicsCategories.newton != 0) && (secondBody.categoryBitMask & PhysicsCategories.apple != 0)){
-            print("newton and apple Contact")
-            
-        }
-        
-    }
-
 }
--- PlaygroundBook/Sources/GameScene.swift
+++ PlaygroundBook/Sources/GameScene.swift
@@ -1,11 +1,159 @@
 import SpriteKit
-class GameScene : SKScene{
+
+struct Sprite {
+    let node : SKSpriteNode
+    let animations : [SKTexture]
+}
+
+struct PhysicsCategories{
+    static let newton : UInt32 = 0x1 << 0
+    static let apple : UInt32 = 0x1 << 1
+    static let ground : UInt32 = 0x1 << 2
+}
+
+class GameScene : SKScene {
+    
+    var newtonSprite : Sprite!
+    //tamanho do device
+    var windowSize : CGSize!
+    
+    override init(size : CGSize) {
+        super.init(size: size)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func sceneDidLoad() {
-        guard let scene = self.scene else { return }
+        self.windowSize = UIScreen.main.bounds.size
         
+        guard let scene = self.scene else { return }
+        scene.backgroundColor = #colorLiteral(red: 0.368627451, green: 0.7333333333, blue: 0.7921568627, alpha: 1)
+        print("gravidade: \(scene.physicsWorld.gravity)")
+        //scene.physicsWorld.gravity = CGVector(dx: 0, dy: -9.8)
+        
+        //carregando a arvore
+        let treeSprite = loadSprite(imageName: "tree", sizePercentage: 0.35)
+        let treeNode = treeSprite.node
+        treeNode.position = CGPoint(x: treeNode.size.width/2, y: treeNode.size.height/2)
+        //adicionando a maca na cena
+        scene.addChild(treeNode)
+        
+        //carregando sprite do newton
+        newtonSprite = loadSprite(imageName: "newton", sizePercentage: 0.3, animated: true, frames: 1)
+        
+        let newtonNode = newtonSprite.node
+        newtonNode.position = CGPoint(x: windowSize.width * 0.6, y:  newtonNode.size.height/2)
+        newtonNode.physicsBody = SKPhysicsBody(texture: newtonSprite.animations[0], size:  newtonNode.size)
+        newtonNode.physicsBody?.affectedByGravity = false
+        //newtonNode.physicsBody?.pinned = true
+        newtonNode.physicsBody?.isDynamic = false
+        newtonNode.physicsBody?.categoryBitMask = PhysicsCategories.newton
+        newtonNode.physicsBody?.collisionBitMask = PhysicsCategories.apple
+        //adicinando o newton na cena
+        scene.addChild(newtonNode)
+        
+        //carregando o sprite da maca
+        let appleSprite = loadSprite(imageName: "apple", sizePercentage: 0.35)
+        let appleNode = appleSprite.node
+        appleNode.position = CGPoint(x: windowSize.width * 0.6, y: treeNode.position.y * 1.2)
+        //adicionando physics body a maca
+        appleNode.physicsBody = SKPhysicsBody(circleOfRadius: appleNode.size.width/2)
+        appleNode.physicsBody?.affectedByGravity = false
+        appleNode.physicsBody?.categoryBitMask = PhysicsCategories.apple
+        appleNode.physicsBody?.collisionBitMask = PhysicsCategories.newton | PhysicsCategories.ground
+        //adicionando a maca na cena
+        scene.addChild(appleNode)
+        self.wiggleAnimation(apple: appleNode)
+        
+        //adicionando o chao
+        let floor = SKShapeNode(rect: CGRect(x: 0, y: 0, width: windowSize.width, height: 1))
+        floor.fillColor = #colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1)
+        floor.position = CGPoint(x: 0, y: 0)
+        print("tamanho do chao: \(floor.frame.size)")
+        print("posicao do chao: \(floor.position)")
+        floor.physicsBody = SKPhysicsBody(rectangleOf: floor.frame.size, center: CGPoint(x: floor.frame.width/2, y: floor.frame.height/2))
+        floor.physicsBody?.affectedByGravity = false
+        floor.physicsBody?.isDynamic = false
+        floor.physicsBody?.categoryBitMask = PhysicsCategories.ground
+        floor.physicsBody?.collisionBitMask = PhysicsCategories.apple
+        
+        scene.addChild(floor)
        
     }
-
+    
+    override func didMove(to view: SKView) {
+        physicsWorld.contactDelegate = self
+    }
+    
+    func loadSprite(imageName : String, sizePercentage : CGFloat, animated: Bool = false, frames: Int = 1) -> Sprite{
+        
+        var textures = [SKTexture]()
+        for i in 0..<frames{
+            let textureName = imageName + String(i) + ".png"
+            print(textureName)
+            let texture = SKTexture(imageNamed: textureName)
+            print(texture)
+            textures.append(texture)
+        }
+        
+        let spriteNode = SKSpriteNode(texture: textures[0])
+        spriteNode.scale(to: CGSize(width: spriteNode.size.width * sizePercentage, height: spriteNode.size.height * sizePercentage))
+        let sprite = Sprite(node: spriteNode, animations: textures)
+        
+        return sprite
+    }
+    
+    
+    func wiggleAnimation(apple : SKSpriteNode){
+        //change anchor point
+        // apple.anchorPoint = CGPoint(x: apple.size.width/2, y: 0)
+        
+        var angle = Float(-50).degreesToRadians
+        let rotationRight = SKAction.rotate(toAngle: CGFloat(angle), duration: 0.3)
+        angle = Float(50).degreesToRadians
+        let rotationLeft = SKAction.rotate(toAngle: CGFloat(angle), duration: 0.35)
+        
+        let ativarGravidade = SKAction.run {
+            apple.physicsBody?.affectedByGravity = true
+        }
+        
+        let sequence = SKAction.sequence([rotationRight, rotationLeft])
+        let rotation = SKAction.repeat(sequence, count: 2)
+        let animation = SKAction.sequence([rotation, ativarGravidade])
+        
+        apple.run(animation)
+        
+    }
     
 }
+
+extension GameScene : SKPhysicsContactDelegate{
+
+    public func didBegin(_ contact: SKPhysicsContact) {
+        print("contato")
+        let bodyA = contact.bodyA.categoryBitMask
+        let bodyB = contact.bodyB.categoryBitMask
+
+        if bodyA == PhysicsCategories.newton && bodyB == PhysicsCategories.apple{
+            newtonSprite.node.run(SKAction.setTexture(newtonSprite.animations[1]))
+        }
+
+        if bodyA == PhysicsCategories.apple && bodyB == PhysicsCategories.newton{
+            newtonSprite.node.run(SKAction.setTexture(newtonSprite.animations[1]))
+        }
+
+    }
+}
+
+extension BinaryInteger {
+    var degreesToRadians: CGFloat { return CGFloat(self) * .pi / 180 }
+}
+
+extension FloatingPoint {
+    var degreesToRadians: Self { return self * .pi / 180 }
+    var radiansToDegrees: Self { return self * 180 / .pi }
+}
+
+
